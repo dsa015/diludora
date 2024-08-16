4e55ba1e6a01b7caa14bf2a9b69cdbebc1b84e8b
Diluxan Sathalingam <diluxan.sathalingam@dnb.no>
2024-08-16 16:31:30 +0200

    mroe login stuff

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,61 +2,57 @@
 import { createUser, loginUser } from "@/pocketbase";
 import { useState } from "react";
 import styles from "./Login.module.scss";
+import { LoginAccountForm } from "@/components/login-form/LoginAccountForm";
+import { CreateAccountForm } from "@/components/login-form/CreateAccountForm";
+import imageSrc from "../../../public/splittetImage.png";
+import Image from "next/image";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [isSuccess, setIsSuccess] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
 
   return (
-    <div className={styles.container}>
-      <form className={styles.form}>
-        <input
-          name="email"
-          type="email"
-          placeholder="Username"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div className={styles.buttonContainer}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "4rem",
+      }}
+    >
+      <div
+        style={{
+          width: "700px",
+          height: "700px",
+          position: "relative",
+        }}
+      >
+        <Image src={imageSrc} fill alt="" />;
+      </div>
+      <div className={styles.container}>
+        <form className={styles.form}>
+          {isLogin ? <CreateAccountForm /> : <LoginAccountForm />}
           <button
-            type="submit"
-            onClick={(e) => {
-              e.preventDefault();
-              loginUser(email, password);
-              setIsSuccess(true);
-              if (isSuccess) {
-                alert("Login successful");
-                window.location.href = ".";
-              } else {
-                alert("Incorrect username or password");
-              }
-            }}
+            className={styles.submitButton}
+            type="button"
+            // onClick={() => {
+            //   !isLogin ?
+            //   createUser(email, password) :
+            //   loginUser(email,password)
+            // }}
           >
-            Login
+            Continue
           </button>
           <button
-            type="submit"
-            onClick={(e) => {
-              e.preventDefault();
-              createUser(email, password);
-              setIsSuccess(true);
-              if (isSuccess) {
-                window.location.href = ".";
-              } else {
-                alert("User already exists");
-              }
-            }}
+            className={styles.createOrSignInButton}
+            type="button"
+            onClick={() => setIsLogin(!isLogin)}
           >
-            Register
+            {!isLogin
+              ? "Dont have an account? Create account"
+              : "Already have an account? Sign in"}
           </button>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
---
+++ src/components/login-form/CreateAccountForm.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useState } from "react";
+
+export const CreateAccountForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <>
+      <input
+        name="email"
+        type="email"
+        placeholder="Email address"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        name="password"
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input name="password" type="password" placeholder="Confirm password" />
+    </>
+  );
+};
