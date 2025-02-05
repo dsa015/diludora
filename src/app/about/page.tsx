b1bd1f9413a9354f3f73211166c91c859895cdd7
dsa015 <diluxan99@hotmail.com>
2025-02-05 22:34:08 +0100

    added stores page + discounts

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,18 @@
 const About = () => {
-  return <h1>About this page</h1>;
+  return (
+    <>
+      <h1>About </h1>
+      <p>
+        Dilu Dora is where I share my recipes that I've learnt over the years.
+        These recipes are mainly from my mother, as she makes some{" "}
+        <em>meaaaaaan</em> dishes 🔥.
+        <br />
+        No in reality I created this page just to keep track of every recipes,
+        how to make them and stuff, so I don't have to call my mom every time I
+        want to make something... and also polish my web development skills.
+      </p>
+    </>
+  );
 };
 
 export default About;
--- src/app/stores/page.tsx
+++ src/app/stores/page.tsx
@@ -1,9 +1,52 @@
-const Stores = () => {
+import { getListOfStores, ScrapedStore } from "@/puppeteer";
+
+const StoreList = ({ names, links, images }: ScrapedStore) => {
+  const storeToLink = names.map((name, index) => {
+    return {
+      [name]: `https://mattilbud.no${links[index]}`,
+    };
+  });
+
   return (
-    <span>
-      <h1>Where to buy stuff?</h1>
-      <p>Can buy them at:</p>
-    </span>
+    <ul>
+      {storeToLink.map((store, index) => {
+        const storeName = Object.keys(store)[0];
+        const storeLink = Object.values(store)[0];
+        return (
+          <li key={index}>
+            <a href={storeLink} target="_blank" rel="noreferrer">
+              <img src={images[index]} alt={storeName} />
+              <h2>{storeName}</h2>
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+const Stores = async () => {
+  const stores = await getListOfStores();
+  // extract the store names
+  const storeNames = stores.map((store) => store.name);
+  const storeLinks = stores.map((store) => store.link);
+  const storeImages = stores.map((store) => store.img);
+
+  return (
+    <>
+      <section>
+        <h1>Where to buy stuff?</h1>
+        <p>
+          Usually your nearest grocery store is your best bet. But I can list a
+          few of them here, where I go
+        </p>
+        <p>
+          Remember to follow on discounts! It's super important. Down here are
+          this weeks discounts for each store
+        </p>
+      </section>
+      <StoreList names={storeNames} links={storeLinks} images={storeImages} />
+    </>
   );
 };
 
